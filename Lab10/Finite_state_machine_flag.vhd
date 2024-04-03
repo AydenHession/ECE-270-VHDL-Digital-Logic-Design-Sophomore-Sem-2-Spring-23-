9ac3dbf85cd61acbdc2eb9a28399907e2379582c
Ayden Hession <165405763+AydenHession@users.noreply.github.com>
2024-04-03 19:57:18 -0400

    Create Finite_state_machine_flag.vhd

---
+++ Lab10/Finite_state_machine_flag.vhd
@@ -0,0 +1,15 @@
+begin
+
+case (state) is
+  when S0 => flag <= '0';
+
+  when S0 => flag <= '0';
+
+  when S0 => flag <= '1';
+
+  when S0 => flag <= '1';
+
+  when others => flag <= '1';
+
+end case;
+end process;
