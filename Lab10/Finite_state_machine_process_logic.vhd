844702f975c9813a1c7059d38953a5a082790191
Ayden Hession <165405763+AydenHession@users.noreply.github.com>
2024-04-03 20:21:14 -0400

    Create Finite_state_machine_process_logic.vhd

---
+++ Lab10/Finite_state_machine_process_logic.vhd
@@ -0,0 +1,35 @@
+
+
+
+    wait for clk_period*2;
+        ain <= "00";
+    wait for clk_period*2;
+        ain <= "00";
+    wait for clk_period*1;
+        ain <= "00";
+    wait for clk_period*1;
+        ain <= "00";
+    wait for clk_period*2;
+        ain <= "00";
+    wait for clk_period*2;
+        ain <= "00";
+    wait for clk_period*2;
+        ain <= "00";
+    wait for clk_period*2;
+        ain <= "00";
+    wait for clk_period*2;
+        ain <= "00";
+    wait for clk_period*2;
+        ain <= "00";
+    wait for clk_period*2;
+        ain <= "00";
+    wait for clk_period*2;
+        ain <= "00";  
+    wait for clk_period*2;
+        ain <= "00";
+
+
+    wait;
+end process;
+
+END;    
